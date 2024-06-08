fa2d2b3ce47273383652cc701ee488c700492cbf
jhonatacaiob <jhonatacaiob@gmail.com>
2024-06-08 01:26:25 -0300

    Adiciona informação de publicação do livro

--- src/gerenciador_livros/models.py
+++ src/gerenciador_livros/models.py
@@ -1,5 +1,7 @@
+from datetime import date
+
 from sqlmodel import Field, Relationship, SQLModel
-from datetime import datetime
+
 
 class Base(SQLModel):
     ...
@@ -14,6 +16,6 @@ class Autor(Base, table=True):
 class Livro(Base, table=True):
     id: int | None = Field(default=None, primary_key=True)
     titulo: str
-    data_publicacao: datetime = Field()
+    data_publicacao: date = Field()
     autor_id: int = Field(default=None, foreign_key='autor.id')
     autor: Autor = Relationship(back_populates='livros')
