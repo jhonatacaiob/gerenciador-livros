44a822ab613f500bb61ec1d704457f90fd6e3360
jhonatacaiob <jhonatacaiob@gmail.com>
2024-06-30 16:46:54 -0300

    Adiciona test para endpoint options de autores

---
+++ tests/test_autores.py
@@ -0,0 +1,43 @@
+from src.gerenciador_livros.models import Autor
+
+from .factories import AutorFactory
+
+
+def test_listar_autor_options_deve_retornar_uma_lista_de_autores(
+    client, session
+):
+    session.bulk_save_objects(AutorFactory.create_batch(5))
+    session.commit()
+
+    response = client.get('/autores/options/')
+
+    for autor in response.context['autores']:
+        assert isinstance(autor, Autor)
+
+
+def test_listar_autores_options_deve_retornar_lista_vazia_quando_nao_ha_autor(
+    client, session
+):
+    response = client.get('/autores/options/')
+
+    assert len(response.context['autores']) == 0
+
+
+def test_listar_autores_options_deve_retornar_todos_os_autores_criados(
+    client, session
+):
+    len_autores = 10
+    session.bulk_save_objects(AutorFactory.create_batch(len_autores))
+    session.commit()
+
+    response = client.get('/autores/options/')
+
+    assert len(response.context['autores']) == len_autores
+
+
+def test_listar_autores_options_deve_retornar_template_options_autores(
+    client, session
+):
+    response = client.get('/autores/options/')
+
+    assert response.template.name == 'options.html'
